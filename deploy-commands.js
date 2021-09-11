8198624f881d25856f41f68c5080b7c700bbf26d
Max <max@zptx.icu>
2021-09-11 23:18:52 +0800

    feat(deploy): add discord activities

--- deploy-commands.js
+++ deploy-commands.js
@@ -11,6 +11,19 @@ const commands = [
 		.setName('ping')
 		.setDescription('Check the bot\'s latency.'),
 	new SlashCommandBuilder()
+		.setName('activity')
+		.setDescription('Start a Discord activity.')
+		.addStringOption(option =>
+			option
+				.setName('activity')
+				.setDescription('The activity to start.')
+				.setRequired(true)
+				.addChoice('YouTube Together', '755600276941176913')
+				.addChoice('Discord Poker Night', '755827207812677713')
+				.addChoice('Chess in the Park', '832012774040141894')
+				.addChoice('Betrayal.io', '773336526917861400')
+				.addChoice('Fishington.io', '814288819477020702')),
+	new SlashCommandBuilder()
 		.setName('menu')
 		.setDescription('Display the interaction menu.')
 		.addStringOption(option =>
