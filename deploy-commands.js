fb5d5c954a79870b98a0917d71bb3b031db5d859
Max <max@zptx.icu>
2021-09-12 00:31:47 +0800

    feat: add activity command (#2)
    * feat: add initial bot files
    
    * fix(lint): eslint github action failing
    
    * docs: add example config file
    
    * docs: add README
    
    * feat(deploy): add discord activities
    
    * feat: set colors for ping command
    
    * fix: add channel option to activity
    
    * chore: add node-fetch to dependencies
    
    * feat: add activity command handler

--- deploy-commands.js
+++ deploy-commands.js
@@ -11,6 +11,24 @@ const commands = [
 		.setName('ping')
 		.setDescription('Check the bot\'s latency.'),
 	new SlashCommandBuilder()
+		.setName('activity')
+		.setDescription('Start a Discord activity.')
+		.addStringOption(option =>
+			option
+				.setName('activity')
+				.setDescription('The activity to start.')
+				.setRequired(true)
+				.addChoice('YouTube Together', '755600276941176913')
+				.addChoice('Discord Poker Night', '755827207812677713')
+				.addChoice('Chess in the Park', '832012774040141894')
+				.addChoice('Betrayal.io', '773336526917861400')
+				.addChoice('Fishington.io', '814288819477020702'))
+		.addChannelOption(option =>
+			option
+				.setName('channel')
+				.setDescription('The voice channel to start the activity in.')
+				.setRequired(true)),
+	new SlashCommandBuilder()
 		.setName('menu')
 		.setDescription('Display the interaction menu.')
 		.addStringOption(option =>
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Require the necessary discord.js classes
-const { Client, Intents, MessageEmbed } = require('discord.js');
+const { Client, Intents, MessageEmbed, VoiceChannel } = require('discord.js');
 const { token } = require('./config.json');
+const fetch = require('node-fetch');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -19,16 +20,51 @@ client.on('interactionCreate', async interaction => {
 				const msg = await interaction.reply({
 					embeds: [
 						new MessageEmbed()
-							.setDescription('Pong! Calculating latency...'),
+							.setDescription('Pong! Calculating latency...')
+							.setColor('BLURPLE'),
 					],
 					fetchReply: true,
 				});
 				await interaction.editReply({
 					embeds: [
 						new MessageEmbed()
-							.setDescription(`Pong! Latency: ${msg.createdTimestamp - interaction.createdTimestamp}ms (API: ${client.ws.ping}ms)`),
+							.setDescription(`Pong! Latency: ${msg.createdTimestamp - interaction.createdTimestamp}ms (API: ${client.ws.ping}ms)`)
+							.setColor('BLURPLE'),
 					],
 				});
+				break;
+			}
+			case 'activity': {
+				if (interaction.options.getChannel('channel') instanceof VoiceChannel) {
+					const channel = interaction.options.getChannel('channel');
+					const result = await fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
+						method: 'POST',
+						body: JSON.stringify({
+							max_age: 0,
+							max_uses: 0,
+							target_application_id: interaction.options.getString('activity'),
+							target_type: 2,
+							temporary: false,
+							validate: null,
+						}),
+						headers: {
+							'Authorization': `Bot ${token}`,
+							'Content-Type': 'application/json',
+						},
+					});
+					const invite = await result.json();
+					await interaction.reply({
+						embeds: [
+							new MessageEmbed()
+								.setDescription(`Your invite link is https://discord.com/invite/${invite.code}`)
+								.setColor('BLURPLE'),
+						],
+						fetchReply: true,
+					});
+				}
+				else {
+					await interaction.reply({ content: 'That isn\'t a voice channel.', ephemeral: true });
+				}
 				break;
 			}
 			default:
