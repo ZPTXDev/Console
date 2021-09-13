5cc6fc771100a0f41295ff21abf1912694d78469
Max <max@zptx.icu>
2021-09-13 09:53:15 +0800

    refactor!: use arrays in embeds

--- embeds.js
+++ embeds.js
@@ -1,24 +1,34 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-	REGISTER: new MessageEmbed()
+	REGISTER: [
+		new MessageEmbed()
 		.setTitle('Registration')
 		.setDescription('You do not have a profile.\nUse the button below to create one.')
 		.setColor('BLURPLE'),
-	REGISTER_SUCCESS: new MessageEmbed()
+	],
+	REGISTER_SUCCESS: [
+		new MessageEmbed()
 		.setTitle('Registration')
 		.setDescription('Success! You can now use all core features.')
 		.setColor('BLURPLE'),
-	REGISTER_FAILURE: new MessageEmbed()
+	],
+	REGISTER_FAILURE: [
+		new MessageEmbed()
 		.setTitle('Registration')
 		.setDescription('Unfortunately, an error occurred during your registration process.\nYou can try again, or inform a developer.')
 		.setColor('BLURPLE'),
-	MENU: new MessageEmbed()
+	],
+	MENU: [
+		new MessageEmbed()
 		.setTitle('Interaction Menu')
 		.setDescription('Choose an option below to continue.')
 		.setColor('BLURPLE'),
-	MENU_PROFILE_DELETE: new MessageEmbed()
+	],
+	MENU_PROFILE_DELETE: [
+		new MessageEmbed()
 		.setTitle('Delete Profile')
 		.setDescription('Are you sure?\nThis action is irreversible and all data you have will be lost.\nUse the button below to proceed.')
 		.setColor('BLURPLE'),
+	],
 };
