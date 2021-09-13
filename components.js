824736a8e3a06edf0195bfdd2c5917cc180be1d7
Max <max@zptx.icu>
2021-09-13 09:52:42 +0800

    feat: add new values to components

--- components.js
+++ components.js
@@ -3,71 +3,97 @@ const { MessageActionRow, MessageSelectMenu, MessageButton } = require('discord.
 module.exports = {
 	REGISTER: [
 		new MessageActionRow()
-		.addComponents(
-			new MessageButton()
-				.setCustomId('register_create')
-				.setLabel('Create')
-				.setStyle('SUCCESS'),
-		),
+			.addComponents(
+				new MessageButton()
+					.setCustomId('register_create')
+					.setLabel('Create')
+					.setStyle('SUCCESS'),
+			),
 	],
 	MENU: [
 		new MessageActionRow()
-		.addComponents(
-			new MessageSelectMenu()
-				.setCustomId('menu_category')
-				.setPlaceholder('Pick a category')
-				.addOptions([
-					{
-						label: 'Profile',
-						description: 'View your profile',
-						value: 'profile',
-					},
-					{
-						label: 'Job',
-						description: 'View your job details',
-						value: 'job',
-					},
-					{
-						label: 'Bank',
-						description: 'View your bank details',
-						value: 'bank',
-					},
-				]),
-		),
+			.addComponents(
+				new MessageSelectMenu()
+					.setCustomId('menu_category')
+					.setPlaceholder('Pick a category')
+					.addOptions([
+						{
+							label: 'Profile',
+							description: 'View your profile',
+							value: 'profile',
+						},
+						{
+							label: 'Job',
+							description: 'View your job details',
+							value: 'job',
+						},
+						{
+							label: 'Bank',
+							description: 'View your bank details',
+							value: 'bank',
+						},
+					]),
+			),
 	],
 	MENU_BANK: [
 		new MessageActionRow()
-		.addComponents(
-			new MessageButton()
-				.setCustomId('menu_back')
-				.setLabel('Back')
-				.setStyle('SECONDARY'),
-		),
+			.addComponents(
+				new MessageButton()
+					.setCustomId('menu_back')
+					.setLabel('Back')
+					.setStyle('SECONDARY'),
+			),
 	],
 	MENU_PROFILE: [
 		new MessageActionRow()
-		.addComponents(
-			new MessageButton()
-				.setCustomId('menu_profile_delete')
-				.setLabel('Delete')
-				.setStyle('DANGER'),
-			new MessageButton()
-				.setCustomId('menu_back')
-				.setLabel('Back')
-				.setStyle('SECONDARY'),
-		),
+			.addComponents(
+				new MessageButton()
+					.setCustomId('menu_profile_delete')
+					.setLabel('Delete')
+					.setStyle('DANGER'),
+				new MessageButton()
+					.setCustomId('menu_back')
+					.setLabel('Back')
+					.setStyle('SECONDARY'),
+			),
+	],
+	MENU_PROFILE_DELETE: [
+		new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('profile_delete')
+					.setLabel('Delete all data')
+					.setStyle('DANGER'),
+				new MessageButton()
+					.setCustomId('menu_back')
+					.setLabel('Back')
+					.setStyle('SECONDARY'),
+			),
 	],
 	MENU_JOB_NOJOB: [
 		new MessageActionRow()
-		.addComponents(
-			new MessageButton()
-				.setCustomId('menu_job_listings')
-				.setLabel('Job Listings')
-				.setStyle('PRIMARY'),
-			new MessageButton()
-				.setCustomId('menu_back')
-				.setLabel('Back')
-				.setStyle('SECONDARY'),
-		),
+			.addComponents(
+				new MessageButton()
+					.setCustomId('menu_job_listings')
+					.setLabel('Job Listings')
+					.setStyle('PRIMARY'),
+				new MessageButton()
+					.setCustomId('menu_back')
+					.setLabel('Back')
+					.setStyle('SECONDARY'),
+			),
+	],
+	MENU_JOB_WITHJOB: [
+		new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('menu_job_resign')
+					.setLabel('Resign')
+					.setStyle('DANGER'),
+				new MessageButton()
+					.setCustomId('menu_back')
+					.setLabel('Back')
+					.setStyle('SECONDARY'),
+			),
 	],
 };
